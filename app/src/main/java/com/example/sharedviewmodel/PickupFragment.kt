9b502d807e5fb96cf62b437715d3e3c8791a8226
Ikechukwu Achom <ikechukwu.achom@kodehauz.com>
2021-10-28 18:45:56 +0100

    set up fragments and data binding

---
+++ app/src/main/java/com/example/sharedviewmodel/PickupFragment.kt
@@ -0,0 +1,41 @@
+package com.example.sharedviewmodel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.sharedviewmodel.databinding.FragmentPickupBinding
+
+
+class PickupFragment : Fragment() {
+    private var binding: FragmentPickupBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentBinding = FragmentPickupBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+            nextButton.setOnClickListener { goToNextScreen() }
+        }
+    }
+
+
+    fun goToNextScreen() {
+        Toast.makeText(activity, "Next", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
